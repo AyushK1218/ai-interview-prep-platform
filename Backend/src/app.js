a42260c50ccacd4fb705a84600c062d95f8af714
Ayush Kanani <kananiayush3@gmail.com>
2026-05-16 11:14:08 +0530

    Update environment configuration and enhance CORS handling

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,14 +2,28 @@ const express = require("express");
 const cookieParser = require("cookie-parser")
 const cors = require("cors")
 
+const allowedOrigins = [
+    "http://localhost:5173",
+    "http://localhost:3000",
+    process.env.FRONTEND_URL
+]
+
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser())
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}))
+app.use(
+    cors({
+        origin: function (origin, callback) {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error("Not allowed by CORS"));
+            }
+        },
+        credentials: true,
+    }),
+);
 
 /* require all the routes here */
 const authRouter = require("./routes/auth.routes")
